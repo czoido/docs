cec47e3cf258247de3d8423f12246c984a40ab46
czoido <mrgalleta@gmail.com>
2021-01-26 16:05:20 +0100

    wip

--- .ci/create_pr.py
+++ .ci/create_pr.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 
-from github import Github
+from github import Github, GithubException
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Release to github')
@@ -16,9 +16,21 @@ if __name__ == "__main__":
     GH_TOKEN = os.getenv("GH_TOKEN")
     gh = Github(GH_TOKEN)
     repo = gh.get_repo(args.repo)
-    pr = repo.create_pull(
-        title="[ci-bot] Updating {} to merge changes in {}".format(
-            args.base, args.head),
-        body="",
-        head=args.head,
-        base=args.base)
+
+    try:
+        pr = repo.create_pull(
+            title="[ci-bot] Updating {} to merge changes in {}".format(
+                args.base, args.head),
+            body="",
+            head=args.head,
+            base=args.base)
+        print(
+            f"Created pull request #{pr.number} ({args.head} => {args.base})")
+    except GithubException as e:
+        if e.status == 422:
+            print(
+                f"A pull request already exists for ({args.head} => {args.base})"
+            )
+        else:
+            print("Failed to create the PR")
+            print(str(e))
